a91f5e5fdeec5d110fad2919b11a0af02625b472
GANAPATHI S <vengatachalag@gmail.com>
2024-12-05 11:09:06 +0530

    Create My class.java

---
+++ My class.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class MyClass implements MyInterface {
+    @Override
+    public void displayMessage() {
+        System.out.println("Hello from MyClass implementing MyInterface!");
+    }
+}
